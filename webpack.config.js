f61a1e182fd2788eaa0d10196b26d42f46e87947
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-03-07 18:00:46 -0300

    chore: change the webpack output config

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].[contenthash].js',
+    clean: true
   },
   resolve: {
     extensions: ['.js', '.jsx']
