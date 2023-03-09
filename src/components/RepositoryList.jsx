b4936bc1a885e568e84017f44e83cf36a0449a1a
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-03-08 22:32:29 -0300

    feat: add RepositoryItem component

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,29 +1,15 @@
+import { RepositoryItem } from "./RepositoryItem";
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar repositório</a>
-        </li>
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
       </ul>
     </section>
   );
