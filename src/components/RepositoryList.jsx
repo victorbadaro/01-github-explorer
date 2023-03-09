ab2e8cba6ec127ecf0c908038e71e783161f4dea
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-03-09 13:59:00 -0300

    feat: get repositories from the github api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
@@ -9,14 +10,20 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
         <RepositoryItem repository={repository} />
       </ul>
     </section>
